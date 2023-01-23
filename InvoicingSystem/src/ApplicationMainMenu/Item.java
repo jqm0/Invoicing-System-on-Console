4ebc4fca513028742498ed60eafeb774a2b2cecf
jqm0 <121156533+jqm0@users.noreply.github.com>
2023-01-22 22:28:35 -0800

    Add Class Item that has diffrent properties and add some methods for class Shop

---
+++ InvoicingSystem/src/ApplicationMainMenu/Item.java
@@ -0,0 +1,10 @@
+package ApplicationMainMenu;
+
+public class Item {
+	public String name;
+	Integer unitPrice ;
+	Integer id;
+	Integer quantity;
+	Integer qtyAmountPrice;
+	
+}
--- InvoicingSystem/src/ApplicationMainMenu/Shop.java
+++ InvoicingSystem/src/ApplicationMainMenu/Shop.java
@@ -1,5 +1,27 @@
 package ApplicationMainMenu;
 
-public class Shop {
+import java.util.ArrayList;
 
+public class Shop {
+	String name;
+	ArrayList<Item> itemList = new ArrayList<Item>();
+
+	public void setShopName(String shopName) {
+		this.name = shopName;
+	}
+
+	public void addItem(Item newItem) {
+		itemList.add(newItem);
+		
+	}
+
+	public void deleteItem(Item itemToBeDeleted ) {
+		for(Item i : itemList) {
+			if(itemToBeDeleted.name.equalsIgnoreCase(i.name)) {
+				itemList.remove(itemToBeDeleted);
+				System.out.println("Item  has been Deleted ");
+			}
+			
+		}
+	}
 }

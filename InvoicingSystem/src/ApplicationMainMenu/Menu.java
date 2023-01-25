b2a7ce127a3baee59ba636f45b6aad9b7484f284
jqm0 <121156533+jqm0@users.noreply.github.com>
2023-01-24 21:43:31 -0800

    complete part of option 7 and update some methods

--- InvoicingSystem/src/ApplicationMainMenu/Menu.java
+++ InvoicingSystem/src/ApplicationMainMenu/Menu.java
@@ -1,5 +1,9 @@
 package ApplicationMainMenu;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Date;
 import java.util.Scanner;
 
 public class Menu {
@@ -8,6 +12,14 @@ public class Menu {
 		Scanner sc = new Scanner(System.in);
 		Shop shopX = new Shop();
 		boolean repeat = true;
+		Integer countCase1 = 0;
+		Integer countCase2 = 0;
+		Integer countCase3 = 0;
+		Integer countCase4 = 0;
+		Integer countCase5 = 0;
+		Integer countCase6 = 0;
+		Integer countCase7 = 0;
+		Integer countCase8 = 0;
 
 		System.out.println("----------------------------------------");
 		System.out.println("----- Welcom to Invoicing System  ------");
@@ -28,6 +40,7 @@ public class Menu {
 				int option = sc.nextInt();
 				switch (option) {
 				case 1:
+					countCase1++;
 					while (repeat2) {
 						System.out.println(
 								"1. Load Data (Items and invoices)\r\n" + "2. Set Shop Name (data should be saved)\r\n"
@@ -63,6 +76,7 @@ public class Menu {
 					}
 					break;
 				case 2:
+					countCase2++;
 					boolean repeat3 = true;
 					while (repeat3) {
 
@@ -108,32 +122,49 @@ public class Menu {
 					}
 					break;
 				case 3:
+					countCase3++;
 					// - Create New Invoice (Invoices should be save/serialized)
-					
+
 					Invoice invoiceA = new Invoice();
 					invoiceA.createNewInvoice();
-					//In this case i have an issue that the menu will repeated infinte times..
-	
-					repeat = false;// i solved this by exiting the program but details of invoice will be saved in txt files..
+					// In this case i have an issue that the menu will repeated infinte times..
+
+					repeat = false;// i solved this by exiting the program but details of invoice will be saved in
+									// txt files..
 					break;
 				case 4:
+					countCase4++;
 					// Report: Statistics (No Of Items, No of Invoices, Total Sales)
 					Report rep = new Report();
 					rep.getReport1();
 					break;
 				case 5:
+					countCase5++;
 					Report rep2 = new Report();
 					rep2.getAllReport();
 					break;
 				case 6:
+					countCase6++;
 					Report rep3 = new Report();
 					System.out.println("Enter invoice number : ");
 					rep3.search(sc.nextInt());
 					break;
 				case 7:
-					System.out.println("Program Statistics");
+					countCase7++;
+					System.out.println("===== Program Statistics for current Run =====");
+					System.out.println("The option 1 has been entered " + countCase1 + " Times " + "\r\n"
+							+ "The option 2 has been entered " + countCase2 + " Times " + "\r\n"
+							+ "The option 3 has been entered " + countCase3 + " Times " + "\r\n"
+							+ "The option 4 has been entered " + countCase4 + " Times " + "\r\n"
+							+ "The option 5 has been entered " + countCase5 + " Times " + "\r\n"
+							+ "The option 6 has been entered " + countCase6 + " Times " + "\r\n"
+							+ "The option 7 has been entered " + countCase7 + " Times " + "\r\n");
+
+					System.out.println("===== Program Statistics for current Run with Previous Run =====");
+
 					break;
 				case 8:
+					countCase8++;
 					System.out.println(" >> Are you sure you want to exit?");
 					if (sc.next().equalsIgnoreCase("yes")) {
 						repeat = false;
@@ -144,6 +175,24 @@ public class Menu {
 			}
 
 		}
+		// Write entered times in text file
+		File counts = new File("C:\\Users\\Lenovo\\eclipse-workspace\\InvoicingSystem\\countEnteredTimes\\count.txt");
+		try {
+			FileWriter fw = new FileWriter(counts);
+			fw.write(countCase1.toString() + "\r\n");
+			fw.write(countCase2.toString() + "\r\n");
+			fw.write(countCase3.toString() + "\r\n");
+			fw.write(countCase4.toString() + "\r\n");
+			fw.write(countCase5.toString() + "\r\n");
+			fw.write(countCase6.toString() + "\r\n");
+			fw.write(countCase7.toString() + "\r\n");
+			fw.write(countCase8.toString() + "\r\n");
+			fw.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		sc.close();
+
 	}
 }
